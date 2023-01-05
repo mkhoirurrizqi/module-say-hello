ef06f22fa5669c09a40ede31b317f279d5bf630f
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2023-01-05 14:38:33 +0700

    major changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mkhoirurrizqi/module-say-hello
+module github.com/mkhoirurrizqi/module-say-hello/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package module_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name + "!"
+func SayHello(name string, country string) string {
+	return "Hello from" + country + ", " + name + "!"
 }
